466c9c665cf45c6b15ce7921b8221e2af4e68447
Ivan Fuentes <fuentescesarivan@gmail.com>
2026-03-26 19:37:24 -0600

    Init

---
+++ src/Application264E08Ejercicio5/Class1.cs
@@ -0,0 +1,13 @@
+﻿public class Figura264E08Ejercicio5
+{
+    public static double Perimetro(double radio){
+        return 2*3.1416*radio;
+    }
+    public static double Area(double radio){
+        return 3.1416*radio*radio;
+    }
+    public static double volumen(double area, double altura){
+        return area*altura;
+    }
+    
+}
---
+++ src/ConsoleApp264E08Ejercicio5/Program.cs
@@ -0,0 +1,9 @@
+﻿double radio = 5;
+double altura = 10;
+double perimetro = Figura264E08Ejercicio5.Perimetro(radio);
+double area = Figura264E08Ejercicio5.Area(radio);
+double volumen= Figura264E08Ejercicio5.volumen(area, altura);
+
+Console.WriteLine($"El perimetro de un circulo de {radio}cm por pi por 2 es {perimetro}cm");
+Console.WriteLine($"El area de un circulo es {radio}cm por radio por pi es {area}cm2");
+Console.WriteLine($"El volumen de un cilindro con un área de la base de {area}cm2 y {altura}cm de altura es {volumen}cm3");
